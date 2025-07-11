89f1c216d4847b93e12721bb856841369a5f7414
kyhlsd <kmyghn@gmail.com>
2025-07-11 16:05:14 +0900

    ClapGame View 생성

---
+++ Week3Assignment/ClapGame/ClapGameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ClapGameViewController.swift
+//  Week3Assignment
+//
+//  Created by 김영훈 on 7/11/25.
+//
+
+import UIKit
+
+class ClapGameViewController: UIViewController {
+
+    @IBOutlet var inputTextField: UITextField!
+    @IBOutlet var numbersLabel: UILabel!
+    @IBOutlet var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
