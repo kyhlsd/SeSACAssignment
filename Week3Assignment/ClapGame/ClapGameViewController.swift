206433d4b5816e196cfcc750291f5a400f1b9bab
kyhlsd <kmyghn@gmail.com>
2025-07-11 16:41:50 +0900

    박수 수 계산

--- Week3Assignment/ClapGame/ClapGameViewController.swift
+++ Week3Assignment/ClapGame/ClapGameViewController.swift
@@ -17,4 +17,40 @@ class ClapGameViewController: UIViewController {
         super.viewDidLoad()
 
     }
+    
+    @IBAction func inputTextFieldDidEndOnExit(_ sender: UITextField) {
+        guard let text = inputTextField.text else { return }
+        
+        // TODO: validate 함수로 묶기
+        let trimmedText = text.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if trimmedText.isEmpty {
+            print("empty")
+            return
+        }
+        guard let number = Int(trimmedText) else {
+            print("숫자만 입력")
+            return
+        }
+        if number <= 0 {
+            print("자연수만 입력")
+            return
+        }
+        
+        var count = 0
+        for i in 1...number {
+            let numberString = String(i)
+            for numberChar in numberString {
+                if ["3", "6", "9"].contains(numberChar) {
+                    count += 1
+                }
+            }
+        }
+        print(count)
+    }
+    
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
 }
