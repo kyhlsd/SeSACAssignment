efe99003e7e38e5dd093666d66e4f9838a687b13
kyhlsd <kmyghn@gmail.com>
2025-07-13 01:07:34 +0900

    Travel View 생성

---
+++ Week3Assignment/Travel/AdTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AdTableViewCell.swift
+//  Week3Assignment
+//
+//  Created by 김영훈 on 7/12/25.
+//
+
+import UIKit
+
+class AdTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Week3Assignment/Travel/TravelTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelTableViewCell.swift
+//  Week3Assignment
+//
+//  Created by 김영훈 on 7/12/25.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
