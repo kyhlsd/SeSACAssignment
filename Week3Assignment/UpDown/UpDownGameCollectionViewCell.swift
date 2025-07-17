690d462bf4fd809d7499656c790632b989a42a30
kyhlsd <kmyghn@gmail.com>
2025-07-17 22:25:37 +0900

    UpDown Views 생성

---
+++ Week3Assignment/UpDown/UpDownGameCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  UpDownGameCollectionViewCell.swift
+//  Week3Assignment
+//
+//  Created by 김영훈 on 7/17/25.
+//
+
+import UIKit
+
+class UpDownGameCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Week3Assignment/UpDown/UpDownHomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UpDownHomeViewController.swift
+//  Week3Assignment
+//
+//  Created by 김영훈 on 7/17/25.
+//
+
+import UIKit
+
+class UpDownHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
