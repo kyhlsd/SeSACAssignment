0192c9a465de73019be6e8602c995eaae006edde
kyhlsd <kmyghn@gmail.com>
2025-07-17 23:35:46 +0900

    UpDown 최대 숫자 설정 및 화면 이동

--- Week3Assignment/UpDown/UpDownGameViewController.swift
+++ Week3Assignment/UpDown/UpDownGameViewController.swift
@@ -7,23 +7,28 @@
 
 import UIKit
 
-class UpDownGameViewController: UIViewController {
+class UpDownGameViewController: UIViewController, Identifying {
 
+    private let targetNumber: Int
+    
+    init(targetNumber: Int) {
+        self.targetNumber = targetNumber
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    init?(coder: NSCoder, targetNumber: Int) {
+        self.targetNumber = targetNumber
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        print(targetNumber)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Week3Assignment/UpDown/UpDownHomeViewController.swift
+++ Week3Assignment/UpDown/UpDownHomeViewController.swift
@@ -9,21 +9,70 @@ import UIKit
 
 class UpDownHomeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet var numberTextField: UITextField!
+    @IBOutlet var warningLabel: UILabel!
+    
+    private enum UpDownNumberError: Error {
+        case empty
+        case nonNatural
+        case outOfRange
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.isHidden = true
+        
+        configureNumberTextField()
     }
-    */
 
+    private func configureNumberTextField() {
+        numberTextField.layer.addBorder([.bottom], color: .white, width: 2)
+    }
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        do {
+            let number = try getValidNumber()
+            presentUpDownGameViewController(number: number)
+        } catch UpDownNumberError.empty {
+            warningLabel.text = "입력창이 비었습니다."
+        } catch UpDownNumberError.nonNatural {
+            warningLabel.text = "자연수만 입력해주세요."
+        } catch UpDownNumberError.outOfRange {
+            warningLabel.text = "1 ~ 1,000의 수만 입력 가능합니다."
+        } catch {
+            warningLabel.text = "알 수 없는 에러가 발생했습니다."
+        }
+    }
+    
+    private func getValidNumber() throws -> Int  {
+        let trimmed = numberTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+        
+        if trimmed.isEmpty {
+            throw UpDownNumberError.empty
+        }
+        
+        guard let number = Int(trimmed) else {
+            throw UpDownNumberError.nonNatural
+        }
+        
+        if number < 1, number > 1000 {
+            throw UpDownNumberError.outOfRange
+        }
+        
+        return number
+    }
+    
+    private func presentUpDownGameViewController(number: Int) {
+        let storyboard = UIStoryboard(name: "UpDown", bundle: nil)
+        let viewController = storyboard.instantiateViewController(identifier: UpDownGameViewController.identifier) { coder -> UpDownGameViewController in
+            return .init(coder: coder, targetNumber: Int.random(in: 1...number)) ?? .init(targetNumber: Int.random(in: 1...number))
+        }
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    // numberTextFieldDidEndOnExit, TapGegsture
+    @IBAction func dismissKeyboard(_ sender: Any) {
+        view.endEditing(true)
+    }
 }
