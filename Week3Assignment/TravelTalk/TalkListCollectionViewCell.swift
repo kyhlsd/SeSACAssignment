a1f0f7218ca278df7f172c239d8b29911e52a0c6
kyhlsd <kmyghn@gmail.com>
2025-07-18 16:33:41 +0900

    TravelTalk 파일 생성

---
+++ Week3Assignment/TravelTalk/TalkListCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TalkListCollectionViewCell.swift
+//  Week3Assignment
+//
+//  Created by 김영훈 on 7/18/25.
+//
+
+import UIKit
+
+class TalkListCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Week3Assignment/TravelTalk/TalkRoomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TalkRoomTableViewCell.swift
+//  Week3Assignment
+//
+//  Created by 김영훈 on 7/18/25.
+//
+
+import UIKit
+
+class TalkRoomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
