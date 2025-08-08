3ba2dfc21e779b1397257a9f0666c51f87fbb5d9
kyhlsd <kmyghn@gmail.com>
2025-08-08 14:43:50 +0900

    글자수, 환율 뷰컨 추가

--- MVVMBasic/MVVMBasic/SceneDelegate.swift
+++ MVVMBasic/MVVMBasic/SceneDelegate.swift
@@ -24,8 +24,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         bmiVC.tabBarItem = UITabBarItem(title: "BMI", image: UIImage(systemName: "waveform.path.ecg"), tag: 1)
         let birthDayVC = BirthDayViewController()
         birthDayVC.tabBarItem = UITabBarItem(title: "생년월일", image: UIImage(systemName: "calendar"), tag: 2)
+        let wordCounterVC = WordCounterViewController()
+        wordCounterVC.tabBarItem = UITabBarItem(title: "글자수", image: UIImage(systemName: "text.quote"), tag: 3)
+        let currencyVC = CurrencyViewController()
+        currencyVC.tabBarItem = UITabBarItem(title: "환율", image: UIImage(systemName: "dollarsign.circle"), tag: 5)
         
-        tabBarController.viewControllers = [ageVC, bmiVC, birthDayVC]
+        tabBarController.viewControllers = [ageVC, bmiVC, birthDayVC, wordCounterVC, currencyVC]
         tabBarController.tabBar.tintColor = .systemBlue
         tabBarController.tabBar.unselectedItemTintColor = .systemGray
         
