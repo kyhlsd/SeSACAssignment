29a443b779a17b98a4011e7e769723b59746c324
kyhlsd <kmyghn@gmail.com>
2025-08-22 17:01:03 +0900

    8주차 과제 프로젝트 생성

---
+++ Week8Assignment/Week8Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week8Assignment
+//
+//  Created by 김영훈 on 8/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#function)
+        view.backgroundColor = .white
+    }
+
+
+}
+
