406f4c7d07c393495f925f8add388dda799b3bc9
kyhlsd <kmyghn@gmail.com>
2025-08-23 18:02:14 +0900

    다마고치 프로젝트 생성

---
+++ Tamagotchi/Tamagotchi/SelectTamagotchi/SelectTamagotchiViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SelectTamagotchiViewController.swift
+//  Tamagotchi
+//
+//  Created by 김영훈 on 8/23/25.
+//
+
+import UIKit
+
+final class SelectTamagotchiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
